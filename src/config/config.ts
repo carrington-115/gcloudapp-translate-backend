329d4d30ee5f96c8edea3d9f66ea811cb1951a92
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-25 12:16:38 +0530

    fixed error

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 const { Translate } = require("@google-cloud/translate").v2;
-console.log("key", process.env.API_KEY);
 const googleTranslateConfig = new Translate({ key: process.env.API_KEY });
 module.exports = googleTranslateConfig;
